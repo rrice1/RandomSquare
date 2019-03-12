126da4d4792306f4a3a1d2b5c9fdf889ceb1973e
Robert Rice <rrice1291@gmail.com>
2019-03-11 21:12:16 -0500

    finished randomSquare assignment

--- RandomSquare/Program.cs
+++ RandomSquare/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomSquare
 {
@@ -6,7 +7,34 @@ namespace RandomSquare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool IsEven(int value)
+            {
+                return value % 2 == 0;
+            }
+            Random rand = new Random();
+            var myRands = new List<int> {};
+            var myRands1 = new List<int> {};
+            var myRands2 = new List<int> {};
+            for (int i = 0; i < 20; i++)
+            {
+                myRands.Add(rand.Next(1,50));
+                myRands1.Add(myRands[i] * myRands[i]);
+            }
+
+            foreach (var num in myRands1)
+            {
+                if (IsEven(num))
+                {
+                    myRands2.Add(num);
+                }
+
+            }
+            foreach (var num in myRands2)
+            {
+                Console.WriteLine(num);
+            }
+
+                Console.ReadLine();
         }
     }
 }
